aaa86aaae39293f28aa72851d97ec013570042a4
Camilo Lozano <camilo.lozano@be-smart.co>
2022-05-13 00:00:48 -0500

    feat: creating component for product list

--- src/app/product/pages/list/list.component.ts
+++ src/app/product/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductCardModel } from '@core-model/product-card.model';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  products: ProductCardModel[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products = [
+      {
+        name: 'Iphone 11',
+        description: 'Apple Ipod Touch 5g 16gb Negro Igual A Nuevo, Completo Unico!',
+        city: 'Capital Federal',
+        isShipping: true,
+        price: 1.981
+      }, {
+        name: 'Iphone 11',
+        description: 'Apple Ipod Touch 5g 16gb Negro Igual A Nuevo, Completo Unico!',
+        city: 'Capital Federal',
+        isShipping: true,
+        price: 1.981
+      }
+    ];
   }
 
 }
