f18b0ffecf3665bc6416b1de821c5776ec07bc86
Camilo Lozano <camilo.lozano@be-smart.co>
2022-05-12 23:59:40 -0500

    feat: creating card product component

--- src/app/shared/component/product-card/product-card.component.ts
+++ src/app/shared/component/product-card/product-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductCardModel } from '@core-model/product-card.model';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
+  @Input() product: ProductCardModel;
 
   constructor() { }
 
